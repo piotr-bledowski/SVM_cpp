f279e3fda656494b1fc265baaab4b11b0eec71bb
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-06-18 19:05:56 +0200

    Hinge Loss and Objective function implemented

--- include/optimization.h
+++
@@ -1,8 +0,0 @@
-//
-// Created by piotr on 18/06/2023.
-//
-
-#ifndef SVM_CPP_OPTIMIZATION_H
-#define SVM_CPP_OPTIMIZATION_H
-
-#endif //SVM_CPP_OPTIMIZATION_H
--- include/svm.h
+++ include/svm.h
@@ -26,4 +26,21 @@ public:
     int predict(std::vector<double> x) {
 
     }
+
+    double objective(std::vector<std::vector<double>> X, std::vector<int> y) {
+        int n = X.size();
+        std::vector<double> losses;
+        for (int i = 0; i < n; i++) {
+            losses.push_back(hingeLoss(X[i], y[i]));
+        }
+
+        return 1.0/n * std::accumulate(losses.begin(), losses.end(), 0.0);
+    }
+
+    double hingeLoss(std::vector<double> x, int y) {
+        double loss = 1 - y*(std::inner_product(w.begin(), w.end(), x.begin(), 0.0) - b);
+        if (loss > 0)
+            return loss;
+        return 0;
+    }
 };
