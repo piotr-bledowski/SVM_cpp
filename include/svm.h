0598fad5306bb7c900cb694475977bb2ccb3845d
Piotr Błędowski <piotr.bledowski.77@gmail.com>
2023-06-19 02:08:19 +0200

    numerical gradient estimation implemented

--- include/svm.h
+++ include/svm.h
@@ -8,18 +8,29 @@
 #endif //SVM_CPP_SVM_H
 
 #include <vector>
+#include <random>
+
+const double DELTA = 0.001;
 
 class SVM {
 private:
-    std::vector<double> w;
-    double b;
+    std::vector<double> weights;
+    double offset;
     double lambda;
 public:
     SVM() {
-
+        offset = 0;
+        lambda = 0;
     }
 
-    void fit(std::vector<std::vector<double>> X, std::vector<int> y) {
+    // T - number of iterations
+    void fit(std::vector<std::vector<double>> X, std::vector<int> y, int T, double lamb) {
+        // SGD
+        lambda = lamb;
+
+        for (int t = 1; t <= T; t++) {
+
+        }
 
     }
 
@@ -27,17 +38,53 @@ public:
 
     }
 
-    double objective(std::vector<std::vector<double>> X, std::vector<int> y) {
+    double learningRate(int t) {
+        return 1.0 / t;
+    }
+
+    std::vector<double> numericalGradient(std::vector<std::vector<double>> X, std::vector<int> y) {
+        std::vector<double> gradient;
+        for (int i = 0; i <= weights.size(); i++) {
+            gradient.push_back(numericalPartialDerivative(X, y, i));
+        }
+        return gradient;
+    }
+
+    double numericalPartialDerivative(std::vector<std::vector<double>> X, std::vector<int> y, size_t i) {
+        if (i < weights.size()) {
+            std::vector<double> w1 = weights;
+            std::vector<double> w2 = weights;
+
+            w1[i] += DELTA;
+            w2[i] -= DELTA;
+
+            return (objective(X, y, w1, offset) - objective(X, y, w2, offset)) / (2 * DELTA);
+        }
+
+        double b = offset;
+
+        return (objective(X, y, weights, b + DELTA) - objective(X, y, weights, b - DELTA)) / (2 * DELTA);
+    }
+
+    double objective(std::vector<std::vector<double>> X, std::vector<int> y, std::vector<double> w, double b) {
         int n = X.size();
         std::vector<double> losses;
         for (int i = 0; i < n; i++) {
-            losses.push_back(hingeLoss(X[i], y[i]));
+            losses.push_back(hingeLoss(X[i], y[i], w, b));
         }
 
-        return 1.0/n * std::accumulate(losses.begin(), losses.end(), 0.0);
+        std::vector<double> squared;
+
+        for (double & i : w) {
+            squared.push_back(i * i);
+        }
+
+        double regularizer = lambda * std::accumulate(squared.begin(), squared.end(), 0.0);
+
+        return 1.0/n * std::accumulate(losses.begin(), losses.end(), 0.0) + regularizer;
     }
 
-    double hingeLoss(std::vector<double> x, int y) {
+    double hingeLoss(std::vector<double> x, int y, std::vector<double> w, double b) {
         double loss = 1 - y*(std::inner_product(w.begin(), w.end(), x.begin(), 0.0) - b);
         if (loss > 0)
             return loss;
